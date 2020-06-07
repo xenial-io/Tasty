bb59662c8ec6c0bf6121fbc2866fb80f1580b3a9
Manuel Grundner <m.grundner@delegate.at>
2020-06-07 13:46:29 +0200

    fix: clear console detection

--- src/Xenial.Tasty/Tasty.Scope.cs
+++ src/Xenial.Tasty/Tasty.Scope.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 using Xenial.Delicious.Metadata;
 using Xenial.Delicious.Reporters;

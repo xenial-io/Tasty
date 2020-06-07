9720850d89f9e8108beb3380b3699afd46c87a54
Manuel Grundner <m.grundner@delegate.at>
2020-06-07 12:16:00 +0200

    build: added basic build

--- build/Tasty.Build/Program.cs
+++ build/Tasty.Build/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
+using static SimpleExec.Command;
+using static Bullseye.Targets;
+using System.Threading.Tasks;
+using Tasty.Build.Helpers;
+
 namespace Tasty.Build
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello Tasty!");
+            XenialVersionInfo version = null;
+
+            Target("version", () =>
+            {
+                version = new XenialVersionInfo();
+                version.PrintVersion();
+            });
+
+            static string logOptions(string target)
+                => $"/maxcpucount /nologo /verbosity:minimal /bl:./artifacts/logs/tasty.{target}.binlog";
+
+            Target("restore", DependsOn("version"),
+                () => RunAsync("dotnet", $"restore {logOptions("restore")}")
+            );
+
+            Target("build", DependsOn("restore"), 
+                () => RunAsync("dotnet", $"build --no-restore {logOptions("build")}")
+            );
+
+            Target("test", DependsOn("build"), () =>
+            {
+
+            });
+
+            Target("pack", DependsOn("test"), 
+                () => RunAsync("dotnet", $"pack src/Xenial.Tasty/Xenial.Tasty.csproj --no-restore --no-build {logOptions("pack")}")
+            );
+
+            Target("default", DependsOn("test"));
+
+            await RunTargetsAndExitAsync(args);
         }
     }
 }

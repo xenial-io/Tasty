92d818f1dc60efc313483f95a323423ce3962d49
Manuel Grundner <m.grundner@delegate.at>
2020-06-30 10:48:36 +0200

    docs: global scope

--- src/Xenial.Tasty/Reporters/Reporters.cs
+++ src/Xenial.Tasty/Reporters/Reporters.cs
@@ -5,7 +5,21 @@ using Xenial.Delicious.Metadata;
 
 namespace Xenial.Delicious.Reporters
 {
+    /// <summary>
+    /// Delegate AsyncTestReporter
+    /// Gets called immediate after the test was executed
+    /// <see cref="Xenial.Tasty.RegisterReporter(AsyncTestReporter)"/>
+    /// </summary>
+    /// <param name="test">The test.</param>
+    /// <returns>Task.</returns>
     public delegate Task AsyncTestReporter(TestCase test);
 
+    /// <summary>
+    /// Delegate AsyncTestSummaryReporter
+    /// Gets called after all tests are executed.
+    /// <see cref="Xenial.Tasty.RegisterReporter(AsyncTestSummaryReporter)"/>
+    /// </summary>
+    /// <param name="tests">The tests.</param>
+    /// <returns>Task.</returns>
     public delegate Task AsyncTestSummaryReporter(IEnumerable<TestCase> tests);
 }

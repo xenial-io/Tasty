420acf49af0de882e994ee0843ceebcc7788e13f
Manuel Grundner <m.grundner@delegate.at>
2020-07-08 12:16:15 +0200

    feat(cli): add cli project stub
    This adds the new project and introduce new dependencies for the cli
    
    #31

---
+++ src/Xenial.Tasty.Cli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace Xenial.Tasty.Tool
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand();
+
+            var interactiveCommand = new Command("interactive")
+            {
+                Handler = CommandHandler.Create(() => Interactive())
+            };
+            interactiveCommand.AddAlias("i");
+            rootCommand.Add(interactiveCommand);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        static Task<int> Interactive()
+        {
+            return Task.FromResult(0);
+        }
+    }
+}

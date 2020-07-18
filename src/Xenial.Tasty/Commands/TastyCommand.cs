7ec06c7e3801e2e3d2b14bffa13fa2965340c551
Manuel Grundner <m.grundner@delegate.at>
2020-07-18 13:01:01 +0200

    refactor: made tasty command internal

--- src/Xenial.Tasty/Commands/TastyCommand.cs
+++ src/Xenial.Tasty/Commands/TastyCommand.cs
@@ -5,9 +5,9 @@ using Xenial.Delicious.Execution;
 
 namespace Xenial.Delicious.Commands
 {
-    public class TastyCommand
+    internal class TastyCommand
     {
-        public TastyCommand(string name, Func<RuntimeContext, Task> command, string? description, bool isDefault = false)
+        internal TastyCommand(string name, Func<RuntimeContext, Task> command, string? description, bool isDefault = false)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
             Command = command ?? throw new ArgumentNullException(nameof(command));
@@ -15,9 +15,9 @@ namespace Xenial.Delicious.Commands
             IsDefault = isDefault;
         }
 
-        public string Name { get; }
-        public Func<RuntimeContext, Task> Command { get; }
-        public string? Description { get; }
-        public bool IsDefault { get; internal set; } = false;
+        internal string Name { get; }
+        internal Func<RuntimeContext, Task> Command { get; }
+        internal string? Description { get; }
+        internal bool IsDefault { get; internal set; } = false;
     }
 }

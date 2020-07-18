461b0af23012e709f6e8e49102df69275f882115
Manuel Grundner <m.grundner@delegate.at>
2020-07-18 12:30:46 +0200

    refactor: moved enumerable extentions to own file

--- src/Xenial.Tasty/Utils/CastExtentions.cs
+++ src/Xenial.Tasty/Utils/CastExtentions.cs
@@ -8,21 +8,6 @@ using Xenial.Delicious.Metadata;
 
 namespace Xenial.Delicious.Utils
 {
-    internal static class EnumerableExtensions
-    {
-        internal static TestOutcome MinOrDefault<TSource>(this IEnumerable<TSource> sequence, Func<TSource, TestOutcome> selector)
-        {
-            if (sequence.Any())
-            {
-                return sequence.Min(selector);
-            }
-            else
-            {
-                return default;
-            }
-        }
-    }
-
     /// <summary>
     /// CastExtentions for fluent casting
     /// </summary>

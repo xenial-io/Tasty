179a7a26bd41f76024fa5840536dc8480b5f5e26
Manuel Grundner <m.grundner@delegate.at>
2020-07-20 10:20:44 +0200

    feat: add studio command

--- src/Xenial.Tasty.Cli/Program.cs
+++ src/Xenial.Tasty.Cli/Program.cs
@@ -12,6 +12,8 @@ using Microsoft.VisualStudio.Threading;
 
 using StreamJsonRpc;
 
+using Terminal.Gui;
+
 using Xenial.Delicious.Protocols;
 
 using static SimpleExec.Command;
@@ -34,6 +36,13 @@ namespace Xenial.Delicious.Cli
             arg.AddAlias("-p");
             interactiveCommand.Add(arg);
             rootCommand.Add(interactiveCommand);
+
+            var studioCommand = new Command("studio")
+            {
+                Handler = CommandHandler.Create<CancellationToken>(Studio)
+            };
+            studioCommand.AddAlias("s");
+            rootCommand.Add(studioCommand);
 
             return await rootCommand.InvokeAsync(args);
         }
@@ -248,5 +257,73 @@ namespace Xenial.Delicious.Cli
                 await waitForInput();
                 resolve();
             });
+
+        static Task<int> Studio(CancellationToken cancellationToken)
+        {
+            Application.Init();
+            var top = Application.Top;
+
+            // Creates the top-level window to show
+            var win = new Window("MyApp")
+            {
+                X = 0,
+                Y = 1, // Leave one row for the toplevel menu
+
+                // By using Dim.Fill(), it will automatically resize without manual intervention
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            top.Add(win);
+
+            // Creates a menubar, the item "New" has a help menu.
+            var menu = new MenuBar(new MenuBarItem[] {
+            new MenuBarItem ("_File", new MenuItem [] {
+                new MenuItem ("_New", "Creates new file", () => {}),
+                new MenuItem ("_Close", "", () => { }),
+                new MenuItem ("_Quit", "", () => { top.Running = false; })
+            }),
+            new MenuBarItem ("_Edit", new MenuItem [] {
+                new MenuItem ("_Copy", "", null),
+                new MenuItem ("C_ut", "", null),
+                new MenuItem ("_Paste", "", null)
+            })
+        });
+            top.Add(menu);
+
+            var login = new Label("Login: ") { X = 3, Y = 2 };
+            var password = new Label("Password: ")
+            {
+                X = Pos.Left(login),
+                Y = Pos.Top(login) + 1
+            };
+            var loginText = new TextField("")
+            {
+                X = Pos.Right(password),
+                Y = Pos.Top(login),
+                Width = 40
+            };
+            var passText = new TextField("")
+            {
+                Secret = true,
+                X = Pos.Left(loginText),
+                Y = Pos.Top(password),
+                Width = Dim.Width(loginText)
+            };
+
+            // Add some controls, 
+            win.Add(
+                // The ones with my favorite layout system
+                login, password, loginText, passText,
+
+                    // The ones laid out like an australopithecus, with absolute positions:
+                    new CheckBox(3, 6, "Remember me"),
+                    new RadioGroup(3, 8, new[] { "_Personal", "_Company" }),
+                    new Button(3, 14, "Ok"),
+                    new Button(10, 14, "Cancel"),
+                    new Label(3, 18, "Press F9 or ESC plus 9 to activate the menubar"));
+
+            Application.Run();
+            return Task.FromResult(1);
+        }
     }
 }

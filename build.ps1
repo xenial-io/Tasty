94667a2a84ef86e0b1f4b7ce3900f156f0c18ce4
Manuel Grundner <m.grundner@delegate.at>
2020-07-24 11:22:04 +0200

    build: powershell and bash

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 $ErrorActionPreference = "Stop";
-dotnet run --project ".\build\Tasty.Build\Tasty.Build.csproj" targets -- $args
+dotnet run --project ".\build\Tasty.Build\Tasty.Build.csproj" -- $args
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-dotnet run --project "./build/Tasty.Build/Tasty.Build.csproj" targets -- "$@"
+dotnet run --project "./build/Tasty.Build/Tasty.Build.csproj" -- "$@"

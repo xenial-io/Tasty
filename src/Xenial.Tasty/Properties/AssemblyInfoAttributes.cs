ef01c3c930e4478225118e8b2bc1ec3eda554f67
Manuel Grundner <m.grundner@delegate.at>
2020-07-30 09:24:07 +0200

    fix: internals visible to

--- src/Xenial.Tasty/Properties/AssemblyInfoAttributes.cs
+++ src/Xenial.Tasty/Properties/AssemblyInfoAttributes.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Xenial.Tasty.Cli")]
 [assembly: InternalsVisibleTo("Xenial.Tasty.Tests")]
-[assembly: InternalsVisibleTo("Xenial.Tasty.Reports.Console")]
+[assembly: InternalsVisibleTo("Xenial.Tasty.Reporters.Console")]
+[assembly: InternalsVisibleTo("Xenial.Tasty.Reporters.Nyan")]

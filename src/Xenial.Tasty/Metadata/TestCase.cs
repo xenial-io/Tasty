3349a9c4ec56f32f6580daa812f0e44a292d1025
hazard999 <rene.grundner@gmail.com>
2020-07-30 11:02:25 +0200

    Revert "Merge branch 'topic/report-helpers-51' of https://github.com/hazard999/Tasty into topic/report-helpers-51"
    This reverts commit 9c6036669499fa6ce1d22dbc1b547b59f3adb5cd, reversing
    changes made to 86293080836872909f41ba2fdbb3aa6d8bfabe24.

--- src/Xenial.Tasty/Metadata/TestCase.cs
+++ src/Xenial.Tasty/Metadata/TestCase.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,21 +10,10 @@ namespace Xenial.Delicious.Metadata
     [System.Diagnostics.DebuggerDisplay("Name: {Name} Outcome: {TestOutcome} Duration: {Duration}")]
     public class TestCase : IExecutable, IForceAble
     {
-        private TestOutcome _Testoutcome = TestOutcome.NotRun;
-
         public string Name { get; internal set; } = string.Empty;
         public TestGroup? Group { get; internal set; }
         public Executable Executor { get; set; } = () => Task.FromResult(true);
-        
-        public TestOutcome TestOutcome
-        {
-            get => _Testoutcome; internal set
-            {
-                _Testoutcome = value;
-                if (_Testoutcome == TestOutcome.Failed) { Debugger.Break(); }
-            }
-        }
-
+        public TestOutcome TestOutcome { get; internal set; } = TestOutcome.NotRun;
         public Exception? Exception { get; internal set; }
         public Func<bool?> IsIgnored { get; internal set; } = () => false;
         public string IgnoredReason { get; internal set; } = string.Empty;

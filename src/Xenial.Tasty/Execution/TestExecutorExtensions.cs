9c488b660c385183a148a82bb0aaa3315c19df81
Manuel Grundner <m.grundner@delegate.at>
2020-07-30 11:54:28 +0200

    chore: extention -> extension

---
+++ src/Xenial.Tasty/Execution/TestExecutorExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xenial.Delicious.Execution
+{
+    public static class TestExecutorExtensions
+    {
+        public static TestExecutor Use(this TestExecutor app, Func<TestExecutionContext, Func<Task>, Task> middleware)
+        {
+            return app.Use(next =>
+            {
+                return context =>
+                {
+                    Func<Task> simpleNext = () => next(context);
+                    return middleware(context, simpleNext);
+                };
+            });
+        }
+
+        public static TestExecutor UseGroup(this TestExecutor app, Func<TestGroupContext, Func<Task>, Task> middleware)
+        {
+            return app.Use(next =>
+            {
+                return context =>
+                {
+                    Func<Task> simpleNext = () => next(context);
+                    return middleware(context, simpleNext);
+                };
+            });
+        }
+
+        public static TestExecutor UseRuntime(this TestExecutor app, Func<RuntimeContext, Func<Task>, Task> middleware)
+        {
+            return app.Use(next =>
+            {
+                return context =>
+                {
+                    Func<Task> simpleNext = () => next(context);
+                    return middleware(context, simpleNext);
+                };
+            });
+        }
+    }
+}

00ffa1f916ccb912e1ca626c0d73eee6cf9bb66b
Manuel Grundner <m.grundner@delegate.at>
2020-08-28 11:01:24 +0200

    refactor: code cleanup

--- src/Xenial.Tasty.Core/Commander.Tasty.cs
+++ src/Xenial.Tasty.Core/Commander.Tasty.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO.Pipes;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
